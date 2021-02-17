2e10fe727020579c59f8bef756e00555a417ed55
NorthOrange <north_orange_@outlook.com>
2021-02-17 11:44:07 +0800

    fixup! version 1.0

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 -   终端 alacritty
 -   zsh
 -   文件管理器 ranger
--   polybar
-
-~~- 窗口渲染器 picom~~
-~~- 窗口管理器 i3wm~~
--- install.sh
+++ install.sh
@@ -5,16 +5,6 @@ mkdir ~/.config/alacritty
 ln ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 
-# rm -rf ~/.config/i3/
-# cp -r ./i3/ ~/.config/i3/
-
-# rm -rf ~/.config/picom/
-# mkdir ~/.config/picom
-# ln ./picom/picom.conf ~/.config/picom/picom.conf
-# 
-# rm -rf ~/.config/polybar/
-# mkdir ~/.config/polybar
-# ln ./polybar/config ~/.config/polybar/config
 
 rm -f ~/.zshrc
 ln ./zshrc/zshrc ~/.zshrc
